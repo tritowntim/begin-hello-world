ba20491f8f0656ade12f80cbfc061760f51cba92
Tim Dussinger <tritowntim@gmail.com>
2019-09-04 21:03:25 -0400

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -17,8 +17,7 @@ let body = `
   <body>
 
     <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
+      Greetings, Earth!
     </h1>
 
     <p class="center-text">
